4cb2b7f1145491c5eafabe8f0cc3b03affbe5093
TyRun555 <tyrun555@gmail.com>
2021-09-07 14:21:51 +0300

    Update AccountController.php

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -4,6 +4,25 @@ namespace app\controllers;
 
 class AccountController extends \yii\web\Controller
 {
+    /**
+     * {@inheritdoc}
+     */
+    #[ArrayShape(['access' => "array", 'verbs' => "array"])]
+    public function behaviors(): array
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+    
     public function actionIndex()
     {
         return $this->render('index');
